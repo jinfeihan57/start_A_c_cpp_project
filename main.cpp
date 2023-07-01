a1f8f8a6c35ed72a2e7a6528df09020394bb4fde
jinfeihan57 <jinfeihan57.gmail.com>
2023-07-01 15:45:55 +0800

    separate hello world stage 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int hello() {
+    std::cout << "hello";
+    return 0;
+}
+
+int world() {
+    std::cout << " world" << std::endl;
+    return 0;
+}
+
 int main() {
-    std::cout << "hello world" << std::endl;
+    // std::cout << "hello world" << std::endl;
+    hello();
+    world();
     return 0;
 }
