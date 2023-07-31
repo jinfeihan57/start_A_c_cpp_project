ce4c3484c59c20a277ae913433d26d8515b14c1e
jinfeihan57 <jinfeihan57.gmail.com>
2023-07-31 23:45:08 +0800

    0731

--- tests/test_hello.cpp
+++ tests/test_hello.cpp
@@ -15,3 +15,13 @@ TEST(IsOdd, IsOdd5) {
     EXPECT_TRUE(IsOdd(5)) << "5 is not odd!";
 
 }
+
+TEST(Sum, IsOdd5) {
+    int a = 10;
+    int b = 11;
+    int c = Sum(a, b);
+    EXPECT_EQ(c, 21);
+    a = 20;
+    b = 23;
+    EXPECT_EQ(Sum(a, b), 43);
+}
---
+++ tests/test_world.cpp
@@ -0,0 +1,8 @@
+#include "world/world.h"
+
+#include "gtest/gtest.h"
+
+TEST(World, World0) {
+    EXPECT_EQ(0, World());
+
+}
